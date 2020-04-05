d866c15e7650d5068efcbb7845fad6b106c4b4e3
clemson tigress <anreeves83@gmail.com>
2020-04-04 22:34:06 -0400

    Homework 5 v(1.2)-Added color code

--- script.js
+++ script.js
@@ -7,19 +7,42 @@ $(document).ready(function() {
       dynamicTime()
     }
 
-
 function dynamicTime() {
     
     var currentTime = moment().format('LLL');
     $("#currentTime").text(currentTime);
     setInterval(dynamicTime, 1000);
-    console.log(currentTime)
-    console.log(dynamicTime)
+    
+    //console.log(dynamicTime());
  } 
 
+//color code blocks based on past/present/future
+var currentHourInt = parseInt(moment().format('H'));
 
+var timeIDs = $('#schedule-table tr[id]')
+      .map(function() {
+        return this.id;
+      })
+      .get();
 
+for (var i = 0; i < timeIDs.length; i++) {
+    var timeIDsInt = parseInt(timeIDs[i]);
+    //console.log(timeIDsInt)
 
+    if (timeIDsInt < currentHourInt) {
+        $('#' + timeIDs[i])
+          .find('textarea')
+          .css('background-color', 'rgb(183, 199, 216)');
+      } else if (timeIDsInt === currentHourInt) {
+        $('#' + timeIDs[i])
+          .find('textarea')
+          .css('background-color', '#fc7c73');
+      } else {
+        $('#' + timeIDs[i])
+          .find('textarea')
+          .css('background-color', '#6ef55c');
+      }
+    }
 
 
 })
